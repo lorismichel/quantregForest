6af522fc539cd9d59d66f2c1763d09a9e04c4501
Nicolai Meinshausen <nicolai@stat.berkeley.edu>
2007-03-08 00:00:00 +0000

    version 0.2-2

--- R/predict.quantregForest.R
+++ R/predict.quantregForest.R
@@ -9,6 +9,9 @@ function(object, newdata= NULL, quantiles= c(0.1,0.5,0.9), ... ) {
   
   x <- newdata
   if(!is.null(x)){
+    if(is.vector(x)){
+      x <- matrix(x,nrow=1)
+    }
     if (nrow(x) == 0) 
       stop("newdata has 0 rows")
     if (any(is.na(x))) 
@@ -27,7 +30,7 @@ function(object, newdata= NULL, quantiles= c(0.1,0.5,0.9), ... ) {
     if (length(cat.new) != length(object$forest$ncat))
       stop("Number of variables in newdata does not match the model.")
     if (!all(object$forest$ncat == cat.new)) 
-      stop("Type of predictors in new data do not match that of the training data.")
+      stop("Type of predictors in new data do not match types of the training data.")
   }
 
   if(!is.null(newdata)){
@@ -43,91 +46,118 @@ function(object, newdata= NULL, quantiles= c(0.1,0.5,0.9), ... ) {
   
   #### Out-of-bag prediction or not ?
     if(is.null(newdata)){
-    origObs <- object$origObs
-    nobs <- length(origObs)
-
-    origNodes <- object$origNodes
-    
-    quant <- matrix(nrow=nobs,ncol=length(quantiles))
-    ntree <- object$ntree
-
-    normalise <- 0
-    weightvec <- rep(0,nobs*nobs)
-    counti <- rep(0,nobs)
-    thres <- 5*.Machine$double.eps
-
-    
-    result <- .C("findweightsinbag",
-                 as.double(as.vector(origNodes)),
-                 as.integer(as.vector(object$inbag)),
-                 weightvec=as.double(weightvec),
-                 as.integer(nobs),
-                 as.integer(ntree),
-                 as.double(thres),
-                 as.integer(counti),
-                 as.integer(normalise),
-                 DUP=FALSE,
-                 PACKAGE="quantregForest")
-
-
-    
-    weights <- matrix(result$weightvec,nrow= nobs)
-
-    ord <- order(origObs)
-    origObs <- origObs[ord]
-    weights <- weights[ord,]
-    cumweights <- apply(weights,2,cumsum)
-    cumweights <- sweep(cumweights,2,cumweights[nobs,],FUN="/")
-
-    for (qc in 1:length(quantiles)){
-      larg <- cumweights<quantiles[qc]
-      wc <- apply(larg,2,sum)+1
-      quant[,qc] <- origObs[wc]
+      origObs <- object$origObs
+      nobs <- length(origObs)
+      
+      origNodes <- object$origNodes
+      
+      quant <- matrix(nrow=nobs,ncol=length(quantiles))
+      ntree <- object$ntree
+      
+      normalise <- 0
+      weightvec <- rep(0,nobs*nobs)
+      counti <- rep(0,nobs)
+      thres <- 5*.Machine$double.eps
+      
+      
+      result <- .C("findweightsinbag",
+                   as.double(as.vector(origNodes)),
+                   as.integer(as.vector(object$inbag)),
+                   weightvec=as.double(weightvec),
+                   as.integer(nobs),
+                   as.integer(ntree),
+                   as.double(thres),
+                   as.integer(counti),
+                   as.integer(normalise),
+                   DUP=FALSE,
+                   PACKAGE="quantregForest")
+      
+      
+      
+      weights <- matrix(result$weightvec,nrow= nobs)
+      
+      ord <- order(origObs)
+      origObs <- origObs[ord]
+      weights <- weights[ord,,drop=FALSE]
+      cumweights <- apply(weights,2,cumsum)
+      cumweights <- sweep(cumweights,2,as.numeric(cumweights[nobs,]),FUN="/")
+      
+      for (qc in 1:length(quantiles)){
+        larg <- cumweights<quantiles[qc]
+        wc <- apply(larg,2,sum)+1
+        ind1 <- which(wc<1.1)
+        indn1 <- which(wc>1.1)
+        quant[ind1,qc] <- rep(origObs[1],length(ind1))
+        quantmax <- origObs[wc[indn1]]
+        quantmin <- origObs[wc[indn1]-1]
+        weightmax <- cumweights[cbind(wc[indn1],indn1)]
+        weightmin <- cumweights[cbind(wc[indn1]-1,indn1)]
+        factor <- numeric(length(indn1))
+        indz <- weightmax-weightmin<10^(-10)
+        factor[indz] <- 0.5
+        factor[!indz] <- (quantiles[qc]-weightmin[!indz])/(weightmax[!indz]-weightmin[!indz])
+        quant[indn1,qc] <- quantmin + factor* (quantmax-quantmin)
+      }
+      
+    }else{
+      origObs <- object$origObs
+      
+      origNodes <- object$origNodes
+      
+      quant <- matrix(nrow=nrow(x),ncol=length(quantiles))
+      nodes <- getnodes(object,x)
+      ntree <- object$ntree
+      
+      nobs <- length(origObs)
+      nnew <- nrow(x)
+      normalise <- 0
+      weightvec <- rep(0,nobs*nnew)
+      counti <- rep(0,nobs)
+      thres <- 5*.Machine$double.eps
+      result <- .C("findweights",
+                   as.double(as.vector(origNodes)),
+                   as.double(as.vector(nodes)),
+                   weightvec=as.double(weightvec),
+                   as.integer(nobs),
+                   as.integer(nnew),
+                   as.integer(ntree),
+                   as.double(thres),
+                   as.integer(counti),
+                   as.integer(normalise),
+                   DUP=FALSE,
+                   PACKAGE="quantregForest")
+      
+      weights <- matrix(result$weightvec,nrow= nobs)
+      
+      ord <- order(origObs)
+      origObs <- origObs[ord]
+      weights <- weights[ord,,drop=FALSE]
+      cumweights <- apply(weights,2,cumsum)
+      cumweights <- sweep(cumweights,2,as.numeric(cumweights[nobs,]),FUN="/")
+      
+      #for (qc in 1:length(quantiles)){
+      #  larg <- cumweights<quantiles[qc]
+      #  wc <- apply(larg,2,sum)+1
+      #  quant[,qc] <- origObs[wc]
+      #}
+      for (qc in 1:length(quantiles)){
+        larg <- cumweights<quantiles[qc]
+        wc <- apply(larg,2,sum)+1
+        ind1 <- which(wc<1.1)
+        indn1 <- which(wc>1.1)
+        quant[ind1,qc] <- rep(origObs[1],length(ind1))
+        quantmax <- origObs[wc[indn1]]
+        quantmin <- origObs[wc[indn1]-1]
+        weightmax <- cumweights[cbind(wc[indn1],indn1)]
+        weightmin <- cumweights[cbind(wc[indn1]-1,indn1)]
+        factor <- numeric(length(indn1))
+        indz <- weightmax-weightmin<10^(-10)
+        factor[indz] <- 0.5
+        factor[!indz] <- (quantiles[qc]-weightmin[!indz])/(weightmax[!indz]-weightmin[!indz])
+        quant[indn1,qc] <- quantmin + factor* (quantmax-quantmin)
+      }
+      
     }
-    
-  }else{
-    origObs <- object$origObs
-    
-    origNodes <- object$origNodes
-    
-    quant <- matrix(nrow=nrow(x),ncol=length(quantiles))
-    nodes <- getnodes(object,x)
-    ntree <- object$ntree
-
-    nobs <- length(origObs)
-    nnew <- nrow(x)
-    normalise <- 0
-    weightvec <- rep(0,nobs*nnew)
-    counti <- rep(0,nobs)
-    thres <- 5*.Machine$double.eps
-    result <- .C("findweights",
-                 as.double(as.vector(origNodes)),
-                 as.double(as.vector(nodes)),
-                 weightvec=as.double(weightvec),
-                 as.integer(nobs),
-                 as.integer(nnew),
-                 as.integer(ntree),
-                 as.double(thres),
-                 as.integer(counti),
-                 as.integer(normalise),
-                 DUP=FALSE,
-                 PACKAGE="quantregForest")
-    
-    weights <- matrix(result$weightvec,nrow= nobs)
-
-    ord <- order(origObs)
-    origObs <- origObs[ord]
-    weights <- weights[ord,]
-    cumweights <- apply(weights,2,cumsum)
-    cumweights <- sweep(cumweights,2,cumweights[nobs,],FUN="/")
-
-    for (qc in 1:length(quantiles)){
-      larg <- cumweights<quantiles[qc]
-      wc <- apply(larg,2,sum)+1
-      quant[,qc] <- origObs[wc]
-    }
-
-  }
   colnames(quant) <- paste("quantile=",quantiles)
   return(quant)
   
