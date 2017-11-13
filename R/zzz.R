7b917c96d80eaf7d2e218b011a522c9ac1334fa4
lorismichel <loris.michel@effixis.ch>
2017-11-13 10:22:43 +0100

    added description of the package

---
+++ R/zzz.R
@@ -0,0 +1,5 @@
+#' @useDynLib quantregForest, .registration = TRUE
+#' @details
+#' implementation of the quantile forest algorithm (Meinshausen, 2006).
+#' @docType package
+#' @name quantregForest
